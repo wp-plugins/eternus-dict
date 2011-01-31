03a2551d9385020763a42ca790c07c9c46a9c2b3
eternus <eternus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-31 09:14:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/eternus-dict/trunk@339125 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ deactivate.php
@@ -0,0 +1,28 @@
+<?php
+/*
+* author : Eternus web d.o.o.
+* Website : http://www.eternus-web.hr
+* Copyright Eternus web all rights reserved
+*/
+/************************************************************************************************************/
+?>
+<?php
+function eternus_dict_deactivate(){
+	global $wpdb;
+
+	$plugin_file = 'eternus_dict/eternus_dict.php';
+	$page = 1;
+?>
+
+<div class="wrap">
+	<h2 style="font-style:normal;">Are you sure you want to deactivate Eternus dict <?=eternus_dict_version?>?</h2>
+	<p><a href="<?=wp_nonce_url('plugins.php?action=deactivate&amp;plugin='.$plugin_file.'&amp;plugin_status=all&amp;paged=' . $page, 'deactivate-plugin_' . $plugin_file)?>">Yes I am sure</a> | <a href="?page=eternus_dict/words.php">No, please back me to Eternus dict home page</a></p>
+
+</div>
+
+
+<?php
+}
+
+//add_action("admin_menu","mlang_config_menu"); 
+?>
---
+++ eternus_dict.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Plugin Name: Eternus dict
+Plugin URI: http://www.eternus-dict.com/
+Description: This plugin allows you to place content anywhere on the web page.
+Version: 1.0
+Author: Eternus web d.o.o.
+Author URI: http://www.eternus-web.hr/
+License: GPLv2
+*/
+
+
+/*  Copyright 2011 Eternus web d.o.o.  (email : info@eternus-dict.com )
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License, version 2, as
+    published by the Free Software Foundation.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with this program; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+# define
+if ( ! defined( 'WPMLANG_PLUGIN_URL' ) )
+	define( 'WPMLANG_FLAG_URL', '/wp-content/plugins/sitepress-multilingual-cms/res/flags/');
+define( 'eternus_dict_version', '1.0');
+
+# ETERNUS DICT FUNCTIONS
+include('functions.php');
+
+# here we check if WPML is installed
+if(existsWPML()){define('ETERNUS_DICT_WPML', true);}else{define('ETERNUS_DICT_WPML', false);}
+
+
+
+# end of define
+# here we take all words for current language
+############################################################
+function take_eternus_dict($lang_code = '') {
+	global $wpdb;
+	if(ETERNUS_DICT_WPML){
+		$word_object = $wpdb -> get_results("SELECT w.word, w.call_key FROM {$wpdb->prefix}icl_languages l, {$wpdb->prefix}eternus_dict w WHERE l.id = w.lang_id AND l.code = '".$lang_code."'");
+	}else{
+		$word_object = $wpdb -> get_results("SELECT w.word, w.call_key FROM {$wpdb->prefix}eternus_dict w WHERE w.lang_id = '0'");
+	}#end else
+
+	$words_array = objectToArray($word_object);
+	if(is_array($words_array)){
+		foreach($words_array AS $key=>$value){
+			$dict[$value['call_key']] = $value['word'];
+		}#end foreach
+	}else{
+		$dict = array();
+	}#end if
+	return $dict;
+}
+#end function
+############################################################
+
+
+# object => array
+############################################################
+function objectToArray($object){
+        if(!is_object($object) && !is_array($object)){return $object;}
+        if(is_object($object ) ){$object = get_object_vars($object);}
+        return array_map('objectToArray', $object);
+}
+#end function
+############################################################
+
+# WP ADMIN MENU SETTINGS
+include('admin_menu.php');
+?>
