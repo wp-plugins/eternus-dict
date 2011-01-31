44c2311e71fa9b3857f3d05d146ba9a80a54d8e4
eternus <eternus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-01-31 09:12:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/eternus-dict/trunk@339123 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin_functions.php
@@ -0,0 +1,8 @@
+<?php
+/*
+* author : Eternus web d.o.o.
+* Website : http://www.eternus-web.hr
+* Copyright Eternus web all rights reserved
+*/
+/************************************************************************************************************/
+?>
---
+++ admin_menu.php
@@ -0,0 +1,43 @@
+<?php
+/*
+* author : Eternus web d.o.o.
+* Website : http://www.eternus-web.hr
+* Copyright Eternus web all rights reserved
+*/
+/************************************************************************************************************/
+?>
+<?php
+
+# MENU SETTINGS
+add_action('admin_menu', 'eternus_dict_add_pages');
+
+# INCLUDE
+include('words.php');
+include('how_to_use.php');
+include('admin_functions.php');
+
+// action function for above hook
+function eternus_dict_add_pages() {
+
+    // Add a new top-level menu (ill-advised):
+    add_menu_page(__('Eternus dict','menu-test'), __('Eternus dict','menu-test'), 'manage_options', 'eternus_dict/words.php', 'eternus_dict_word' );
+
+	// Add a submenu to the custom top-level menu:
+    add_submenu_page('eternus_dict/words.php', __('How to use?','how_to_use'), __('How to use?','how_to_use'), 'manage_options', 'eternus_dict/how_to_use.php', 'mt_sublevel_page');
+
+	// Add a submenu to the custom top-level menu:
+    add_submenu_page('eternus_dict/words.php', __('Deactivate','Deactivate'), __('Deactivate','Deactivate'), 'manage_options', 'eternus_dict/deactivate.php', 'mt_sublevel_page2');
+
+}
+
+// mt_sublevel_page() displays the page content for the first submenu
+// of the custom Test Toplevel menu
+function mt_sublevel_page() {
+    eternus_dict_how_to_use();
+}#end function
+
+function mt_sublevel_page2() {
+	include('deactivate.php');
+	eternus_dict_deactivate();
+}#end function
+?>
